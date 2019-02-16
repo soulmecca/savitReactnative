e1688e90ec0dbc591dca46963cc05ba5a0edf34d
soulmecca <soulmecca@gmail.com>
2019-02-15 22:56:59 -0500

    preload Assets and Font and Icons

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Iconicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+class App extends React.Component {
+   state = {
+      loading: true
+   };
+   render() {
+      if (this.state.loading) {
+         return (
+            <AppLoading
+               startAsync={this.loadAsssetsAsync}
+               onError={this.handleLoadingError}
+               onFinish={this.handleFinishLoading}
+            />
+         );
+      }
+      return (
+         <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+         </View>
+      );
+   }
+
+   loadAsssetsAsync = async () => {
+      return Promise.all([
+         Assets.loadAsync([
+            require("./assets/images/logo.png"),
+            require("./assets/images/logo-white.png"),
+            require("./assets/images/profile.jpg"),
+            require("./assets/images/photoPlaceholder.png")
+         ]),
+         Font.loadAsync([...Ionicons.font, ...MaterialIcons.font])
+      ]);
+   };
+
+   handleFinishLoading = async () => {
+      this.setState({
+         loading: false
+      });
+   };
+
+   handleLoadingError = error => {
+      console.log(error);
+   };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center"
+   }
 });
+
+export default App;
